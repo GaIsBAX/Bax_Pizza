ac5161b8bbb5a59ed284e579f948789a2e023386
Bax <gais.bax@gmail.com>
2024-07-15 14:24:09 +0300

    feat: Hide popup when clicked past

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setSort } from "../redux/slices/filterSlice";
 
@@ -15,14 +15,26 @@ const Sort = () => {
   const sort = useSelector((state) => state.filter.sort);
   const dispatch = useDispatch();
   const [open, setOpen] = useState(false);
+  const sortRef = useRef();
 
   const onClickListItem = (obj) => {
     dispatch(setSort(obj));
     setOpen(!open);
   };
 
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (!event.composedPath().includes(sortRef.current)) {
+        setOpen(false);
+      }
+    };
+
+    document.body.addEventListener("click", handleClickOutside);
+    return () => document.body.removeEventListener("click", handleClickOutside);
+  }, []);
+
   return (
-    <div className="sort">
+    <div ref={sortRef} className="sort">
       <div className="sort__label">
         <svg
           width="10"
